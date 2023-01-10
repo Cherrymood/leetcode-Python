08c32469fd720251804a28ca14fec1bd52665f6d
Valeriia <olxlerasport@gmail.com>
2023-01-10 14:23:28 -0800

    Added solution1470

---
+++ solution1470.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: list[int], n: int) -> list[int]:
+
+        x = nums[0:n]
+        y = nums[n+1:-1]
+
+        answer = []
+
+        for index in range(len(nums)):
+            answer.append(nums[index])
+                
